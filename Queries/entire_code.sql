7248776d3aaca1a81297f99bcf38b90218640e1b
unknown <emjchou@gmail.com>
2022-02-13 13:36:15 -0800

    rearrange files, add table, end of 7.3.4

--- Queries/entire_code.sql
+++ Queries/entire_code.sql
@@ -176,5 +176,17 @@ WHERE de.to_date = ('9999-01-01');
 --check table
 SELECT * FROM current_emp;
 
-
+--join the current_emp and dept_emp tables
+--to count employees by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO employee_by_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+--above, we used COUNT instead of SUM because the employee numbers would have just been added
+--SUM adds all employee numbers together
+--COUNT counts the number of employee numbers (thus the number of employees in the list)
+SELECT * FROM employee_by_dept
 
