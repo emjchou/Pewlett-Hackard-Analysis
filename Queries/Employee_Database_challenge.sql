c203e645c8e6652249d081d9c6a66aa954da779b
unknown <emjchou@gmail.com>
2022-02-13 18:03:04 -0800

    updating queries file

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -34,3 +34,30 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC
+
+--Deliverable 2: employees for mentorship program
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date,
+	de.from_date, de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+	LEFT JOIN dept_emp AS de
+	ON (e.emp_no = de.emp_no)
+	LEFT JOIN titles as t
+	ON (e.emp_no = t.emp_no)
+WHERE de.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no;
+
+--check tables
+SELECT * FROM retirement_titles;
+SELECT * FROM unique_titles;
+SELECT * FROM retiring_titles;
+SELECT * FROM mentorship_eligibility;
+
+--look at the count for mentorship eligibility
+SELECT COUNT(emp_no), title FROM mentorship_eligibility
+GROUP BY title;
+
+--look at the total count of those retiring
+SELECT SUM(count) FROM retiring_titles
